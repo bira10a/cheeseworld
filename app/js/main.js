cc5caa3b9f2c4e301a4dec8dc443f783dcf653ce
Rodchenko Nikita <bira10a@gmail.com>
2023-02-16 16:42:03 +0300

    added dark theme and added header menu

--- app/js/main.js
+++ app/js/main.js
@@ -4,11 +4,14 @@ const burger = document.querySelector('.burger'),
       btnCloseMemu = document.querySelector('.header-nav__btn'),
       btnCloseMemuLineOne = document.querySelector('.header-nav__btn-one'),
       btnCloseMemuLineTwo = document.querySelector('.header-nav__btn-two'),
-      body = document.querySelector('body');
+      body = document.querySelector('body'),
+      content = document.querySelector('.content');
 
 burger.addEventListener('click', () => {
   headerMenu.classList.add('header-nav__inner--active');
 
+  body.classList.add('body-lock');
+  content.classList.add('lock');
 
   // setTimeout(() => {
   //   btnCloseMemuLineOne.style.animation = 'btnLineOn 1s linear forwards';
@@ -24,6 +27,9 @@ btnCloseMemu.addEventListener('click', () => {
   // btnCloseMemuLineTwo.style.animation = 'btnLineOff 1s linear forwards';
 
   headerMenu.classList.remove('header-nav__inner--active');
+  content.classList.remove('lock');
+
+  body.classList.remove('body-lock');
   
   headerMenuItem.forEach(i => {
     i.style.animation = 'opacity 0.4s linear forwards';
@@ -53,88 +59,88 @@ headerCheckbox.addEventListener('change', () => {
 
 ///////////////////////////////////////// GSAP
 
-// gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 
-// if (ScrollTrigger.isTouch !== 1) {
+if (ScrollTrigger.isTouch !== 1) {
 
-//   ScrollSmoother.create({
-//     wrapper: '.wrapper',
-//     content: '.content',
-//     smooth: 1.5,
-//     effects: true
-//   })
+  ScrollSmoother.create({
+    wrapper: '.wrapper',
+    content: '.content',
+    smooth: 1.5,
+    effects: true
+  })
 
-//   gsap.fromTo('.header', { opacity: 1 }, {
-//     opacity: 0,
-//     scrollTrigger: {
-//       trigger: '.header',
-//       start: 'center',
-//       end: '650',
-//       scrub: true
-//     }
-//   })
+  gsap.fromTo('.header', { opacity: 1 }, {
+    opacity: 0,
+    scrollTrigger: {
+      trigger: '.header',
+      start: 'center',
+      end: '650',
+      scrub: true
+    }
+  })
 
-//   let itemsL = gsap.utils.toArray('.gallery__left')
+  let itemsL = gsap.utils.toArray('.gallery__left')
 
-//   itemsL.forEach(item => {
-//     gsap.fromTo(item, { opacity: 0, x: -100 }, {
-//       opacity: 1, x: 0,
-//       scrollTrigger: {
-//         trigger: item,
-//         start: '-650',
-//         end: '-100',
-//         scrub: true
-//       }
-//     })
-//   })
+  itemsL.forEach(item => {
+    gsap.fromTo(item, { opacity: 0, x: -100 }, {
+      opacity: 1, x: 0,
+      scrollTrigger: {
+        trigger: item,
+        start: '-650',
+        end: '-100',
+        scrub: true
+      }
+    })
+  })
   
-//   itemsL.forEach(item => {
-//     gsap.fromTo(item, { opacity: 1, x:  0}, {
-//       opacity: 0, x: -100,
-//       scrollTrigger: {
-//         trigger: item,
-//         start: 'top',
-//         end: '400',
-//         scrub: true
-//       }
-//     })
-//   })
+  // itemsL.forEach(item => {
+  //   gsap.fromTo(item, { opacity: 1, x:  0}, {
+  //     opacity: 0, x: -100,
+  //     scrollTrigger: {
+  //       trigger: item,
+  //       start: 'top',
+  //       end: '400',
+  //       scrub: true
+  //     }
+  //   })
+  // })
 
-//   let itemsR = gsap.utils.toArray('.gallery__right')
+  let itemsR = gsap.utils.toArray('.gallery__right')
 
-//   itemsR.forEach(item => {
-//     gsap.fromTo(item, { opacity: 0, x: 50 }, {
-//       opacity: 1, x: 0,
-//       scrollTrigger: {
-//         trigger: item,
-//         start: '-550',
-//         end: 'top',
-//         scrub: true
-//       }
-//     })
-//   })
-
-
-
-//   gsap.fromTo('.assortment__list', { x: -40 }, {
-//     x: -400,
-//     scrollTrigger: {
-//       trigger: '.assortment__list',
-//       start: '-850',
-//       end: 'bottom',
-//       scrub: true
-//     }
-//   })
-
-//   gsap.fromTo('.assortment__list', { opacity: 1 }, {
-//     opacity: 0,
-//     scrollTrigger: {
-//       trigger: '.assortment__list',
-//       start: '-200',
-//       end: '200',
-//       scrub: true
-//     }
-//   })
+  itemsR.forEach(item => {
+    gsap.fromTo(item, { opacity: 0, x: 50 }, {
+      opacity: 1, x: 0,
+      scrollTrigger: {
+        trigger: item,
+        start: '-550',
+        end: 'top',
+        scrub: true
+      }
+    })
+  })
 
 
-// }
+
+  gsap.fromTo('.assortment__list', { x: -40 }, {
+    x: -400,
+    scrollTrigger: {
+      trigger: '.assortment__list',
+      start: '-850',
+      end: 'bottom',
+      scrub: true
+    }
+  })
+
+  gsap.fromTo('.assortment__list', { opacity: 1 }, {
+    opacity: 0,
+    scrollTrigger: {
+      trigger: '.assortment__list',
+      start: '-200',
+      end: '200',
+      scrub: true
+    }
+  })
+
+
+}
