8f65e0f23f5697821039d225c600d043a4df2ef7
Rodchenko Nikita <bira10a@gmail.com>
2023-08-28 16:20:16 +0300

    job ysa 28.08.23

--- app/js/main.js
+++ app/js/main.js
@@ -292,4 +292,64 @@ if (ScrollTrigger.isTouch !== 1) {
   })
 
 
+  ////////////////////////
+
+  const contactForm = () => {
+    const input = document.querySelectorAll('.contact-us__input'),
+          spanText = document.querySelectorAll('.contact-us__label');
+
+    input.forEach(i => {
+      i.addEventListener('input', () => {
+        if (i.value != '') {
+          i.classList.add('contact-us__input--focus');
+        } else {
+          i.classList.remove('contact-us__input--focus');
+        }
+
+        if (i.value.match(/\d/g) && i.getAttribute('name') != 'phone') {
+          i.classList.add('contact-us__input--erorr');
+          spanText.forEach(i => {
+          i.textContent = 'Введите корректные значения';
+          });
+          i.style.borderColor = 'red';
+        } else {
+          spanText.forEach(i => {
+            i.textContent = '';
+          });
+          i.style.borderColor = '';
+        }
+      });
+
+    });
+
+    // https://webkaa.ru/javascript/maska-nomera-telefona-js
+    var eventCalllback = function (e) {
+      var el = e.target,
+        clearVal = el.dataset.phoneClear,
+        pattern = el.dataset.phonePattern,
+        matrix_def = "+7(___) ___-__-__",
+        matrix = pattern ? pattern : matrix_def,
+        i = 0,
+        def = matrix.replace(/\D/g, ""),
+        val = e.target.value.replace(/\D/g, "");
+      if (clearVal !== 'false' && e.type === 'blur') {
+        if (val.length < matrix.match(/([\_\d])/g).length) {
+          e.target.value = '';
+          return;
+        }
+      }
+      if (def.length >= val.length) val = def;
+      e.target.value = matrix.replace(/./g, function (a) {
+        return /[_\d]/.test(a) && i < val.length ? val.charAt(i++) : i >= val.length ? "" : a
+      });
+    }
+    var phone_inputs = document.querySelectorAll('[data-phone-pattern]');
+    for (let elem of phone_inputs) {
+      for (let ev of ['input', 'blur', 'focus']) {
+        elem.addEventListener(ev, eventCalllback);
+      }
+    }
+  };
+  contactForm();
+
 }
