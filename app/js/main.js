e74673312f588145ff5d2c76a11b8f4fd6c15c9b
bira10a <94951002+bira10a@users.noreply.github.com>
2024-01-30 16:31:36 +0300

    проверка 30.01.2024

--- app/js/main.js
+++ app/js/main.js
@@ -294,72 +294,7 @@ if (ScrollTrigger.isTouch !== 1) {
 
   ////////////////////////
 
-  const contactForm = () => {
-    const input = document.querySelectorAll('.contact-us__input'),
-          spanText = document.querySelectorAll('.contact-us__label'),
-          form = document.querySelector('.contact-us__form');
 
-    form.addEventListener('submit', (e) => {
-      e.preventDefault();
-
-      input.forEach(i => {
-        if (i.classList.contains('.contact-us__input--erorr')) {
-          console.log('wooo');
-        } else {
-          console.log(i.classList.contains('.contact-us__input--erorr'));
-        }
-      });
-
-      
-    });
-    
-    input.forEach(i => {
-      i.addEventListener('input', () => {
-        if (i.value != '') {
-          i.classList.add('contact-us__input--focus');
-        } else {
-          i.classList.remove('contact-us__input--focus');
-        }
-
-        if (i.value.match(/\d/g) && i.getAttribute('name') != 'phone') {
-          i.classList.add('contact-us__input--erorr');
-          i.style.borderColor = 'red';
-        } else {
-          i.style.borderColor = '';
-        }
-      });
-
-    });
-
-    // https://webkaa.ru/javascript/maska-nomera-telefona-js
-    var eventCalllback = function (e) {
-      var el = e.target,
-        clearVal = el.dataset.phoneClear,
-        pattern = el.dataset.phonePattern,
-        matrix_def = "+7(___) ___-__-__",
-        matrix = pattern ? pattern : matrix_def,
-        i = 0,
-        def = matrix.replace(/\D/g, ""),
-        val = e.target.value.replace(/\D/g, "");
-      if (clearVal !== 'false' && e.type === 'blur') {
-        if (val.length < matrix.match(/([\_\d])/g).length) {
-          e.target.value = '';
-          return;
-        }
-      }
-      if (def.length >= val.length) val = def;
-      e.target.value = matrix.replace(/./g, function (a) {
-        return /[_\d]/.test(a) && i < val.length ? val.charAt(i++) : i >= val.length ? "" : a
-      });
-    }
-    var phone_inputs = document.querySelectorAll('[data-phone-pattern]');
-    for (let elem of phone_inputs) {
-      for (let ev of ['input', 'blur', 'focus']) {
-        elem.addEventListener(ev, eventCalllback);
-      }
-    }
-  };
-  contactForm();
 
 ////////////////////////////////////////////////
   const Spoiler = () => {
@@ -395,3 +330,73 @@ if (ScrollTrigger.isTouch !== 1) {
   footerCopy();
 
 }
+
+
+const contactForm = () => {
+  const input = document.querySelectorAll('.contact-us__input'),
+        spanText = document.querySelectorAll('.contact-us__label'),
+        form = document.querySelector('.contact-us__form');
+
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    input.forEach(i => {
+      if (i.classList.contains('.contact-us__input--erorr')) {
+        console.log('wooo');
+      } else {
+        console.log(i.classList.contains('.contact-us__input--erorr'));
+      }
+    });
+
+    
+  });
+  
+  input.forEach(i => {
+    i.addEventListener('input', () => {
+      if (i.value != '') {
+        i.classList.add('contact-us__input--focus');
+      } else {
+        i.classList.remove('contact-us__input--focus');
+      }
+
+      if (i.value.match(/\d/g) && i.getAttribute('name') != 'phone') {
+        i.classList.add('contact-us__input--erorr');
+        i.style.borderColor = 'red';
+      } else {
+        i.style.borderColor = '';
+      }
+    });
+
+  });
+
+  // https://webkaa.ru/javascript/maska-nomera-telefona-js
+  // var eventCalllback = function (e) {
+  //   var el = e.target,
+  //     clearVal = el.dataset.phoneClear,
+  //     pattern = el.dataset.phonePattern,
+  //     matrix_def = "+7(___) ___-__-__",
+  //     matrix = pattern ? pattern : matrix_def,
+  //     i = 0,
+  //     def = matrix.replace(/\D/g, ""),
+  //     val = e.target.value.replace(/\D/g, "");
+  //   if (clearVal !== 'false' && e.type === 'blur') {
+  //     if (val.length < matrix.match(/([\_\d])/g).length) {
+  //       e.target.value = '';
+  //       return;
+  //     }
+  //   }
+  //   if (def.length >= val.length) val = def;
+  //   e.target.value = matrix.replace(/./g, function (a) {
+  //     return /[_\d]/.test(a) && i < val.length ? val.charAt(i++) : i >= val.length ? "" : a
+  //   });
+  // }
+  // var phone_inputs = document.querySelectorAll('[data-phone-pattern]');
+  // for (let elem of phone_inputs) {
+  //   for (let ev of ['input', 'blur', 'focus']) {
+  //     elem.addEventListener(ev, eventCalllback);
+  //   }
+  // }
+
+
+};
+contactForm();
